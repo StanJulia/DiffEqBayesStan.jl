9215e9f6f2b5026cea7820bbcaca3a6e62509e08
Rob J Goedman <goedman@icloud.com>
2021-11-09 16:30:27 -0700

    Rel 1.1.0

--- src/DiffEqBayesStan.jl
+++ src/DiffEqBayesStan.jl
@@ -16,6 +16,41 @@ module DiffEqBayesStan
     include("stan_string.jl")
     include("stan_inference.jl")
 
-    export stan_inference
+    """
+
+    # debs_path
+
+    Relative path using the StatisticalRethinking src/ directory.
+
+    ### Example to get access to the data subdirectory
+    ```julia
+    debs_path("..", "data")
+    ```
+
+    Note that in the projects, e.g. StatisticalRethinkingStan.jl and StatisticalRethinkingTuring.jl, the
+    DrWatson approach is a better choics, i.e: `sr_datadir(filename)`
+
+    """
+    debs_path(parts...) = normpath(joinpath(src_path, parts...))
+
+    # DrWatson extension
+    """
+
+    # debs_datadir
+
+    Relative path using the StatisticalRethinking src/ directory.
+
+    ### Example to access `Howell1.csv` in StatisticalRethinking:
+    ```julia
+    df = CSV.read(sr_datadir("Howell1.csv"), DataFrame)
+    ```
+    """
+    debs_datadir(parts...) = debs_path("..", "data", parts...)
+
+
+    export
+        stan_inference,
+        debs_path,
+        debs_datadir
     
 end # module
