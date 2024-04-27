faddd583dc04d662e84703f09fde03a7b6fe505c
Rob J Goedman <goedman@icloud.com>
2024-04-27 11:48:41 -0400

    Rel 3.2.1

--- .github/scripts/stan.sh
+++ .github/scripts/stan.sh
@@ -1,7 +1,7 @@
-JULIA_CMDSTAN_HOME="$HOME/cmdstan-2.28.2/"
+JULIA_CMDSTAN_HOME="$HOME/cmdstan-2.34.1/"
 OLDWD=`pwd`
 cd ~
-wget https://github.com/stan-dev/cmdstan/releases/download/v2.28.2/cmdstan-2.28.2.tar.gz
-tar -xzpf cmdstan-2.28.2.tar.gz
+wget https://github.com/stan-dev/cmdstan/releases/download/v2.34.1/cmdstan-2.34.1.tar.gz
+tar -xzpf cmdstan-2.34.1.tar.gz
 make -C $JULIA_CMDSTAN_HOME build
 cd $OLDWD
